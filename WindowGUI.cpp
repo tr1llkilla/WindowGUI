82d7ebfc96e96e4b640b31cde01f33eeeaa3a5bd
tr1llkilla <81764173+tr1llkilla@users.noreply.github.com>
2024-01-10 01:24:42 -0700

    Update WindowGUI.cpp

--- WindowGUI.cpp
+++ WindowGUI.cpp
@@ -1,3 +1,16 @@
+﻿/*
+Copyright (C) 2023 tr1llkilla
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under legal conditions.
+Author:
+tr1llkilla
+
+Author's note:
+This program is unfinished, but does the trick to get the job done manually
+Feel free to credit this work in any future registry forensic or data analysis works!
+*/
+
 #include <Windows.h>
 #include <string>
 #include <bcrypt.h>
